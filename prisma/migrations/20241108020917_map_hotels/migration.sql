d26c9969da5a3e89e959f381c5533901651b796a
Hashem Abdallah <hashimmeko@hotmail.com>
2024-11-10 18:19:40 +0300

    Fixing Seed migrations

--- prisma/migrations/20241108020917_map_hotels/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `qrCode` to the `rooms` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "rooms" ADD COLUMN     "qrCode" TEXT NOT NULL;
